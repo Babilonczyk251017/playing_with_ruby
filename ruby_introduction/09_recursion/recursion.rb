7efaec1e92cc9369ca78aa6a6476926c36cdbb14
Jan Piotrzkowski <piotrzkowski.jan@gmail.com>
2020-04-08 16:03:45 +0200

    Ruby selfstudy 08.04

--- ruby_introduction/09_recursion/recursion.rb
+++ ruby_introduction/09_recursion/recursion.rb
@@ -1,7 +1,6 @@
 
 
 #Simple recursion example
-
 def recurse(r)
   return r if r >10;
   puts r
@@ -10,3 +9,14 @@ def recurse(r)
 end
 
 puts(recurse 1)
+
+#Fibonacci recursion (finding nth fibonacci number)
+def Fibonacci(f)
+  return f if f < 2
+  if f >= 2
+    Fibonacci(f - 1) + Fibonacci(f - 2)
+  end
+end
+
+fibo = Fibonacci(18)
+puts "Fibo: #{fibo}"
